6e237cb55a52c2bf3d6209e473c4c5f9608715d8
James <jamesbridenthal@gmail.com>
2019-12-09 21:37:10 -0500

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  version: '0.0.3',
-  buildDate: 'Mon Dec 09 2019 19:37:17 GMT-0500 (Eastern Standard Time)'
+  version: '0.0.4',
+  buildDate: 'Mon Dec 09 2019 21:36:27 GMT-0500 (Eastern Standard Time)'
  };
